010b4995e07dedb0b2562e4a50c00705b88d5835
Switt Kongdachalert <switt1995@yahoo.com>
2018-05-11 21:39:02 +0700

    Initial Commit

---
+++ Colyseus-ObjC/Colyseus_ObjC.h
@@ -0,0 +1,19 @@
+//
+//  Colyseus_ObjC.h
+//  Colyseus-ObjC
+//
+//  Created by Switt Kongdachalert on 11/5/18.
+//  Copyright © 2018 Switt's Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Colyseus_ObjC.
+FOUNDATION_EXPORT double Colyseus_ObjCVersionNumber;
+
+//! Project version string for Colyseus_ObjC.
+FOUNDATION_EXPORT const unsigned char Colyseus_ObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Colyseus_ObjC/PublicHeader.h>
+
+
